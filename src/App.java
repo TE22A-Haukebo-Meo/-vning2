4326a348b8120c9d9193715f29c1ca4bfadc8fd7
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-12-10 11:04:21 +0100

    del 1 o 2 klara o lite till

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
         String[] toys = {"Bil", "Dinosaurie", "Superman", "Lego", "Nerf"};
         for (String string : toys) {
             System.out.println(string);
+        }
+        String[] comrade = {"Tim", "Tea", "Jacob", "Aleksandra", "MandePOON"};
+        Random rand = new Random();
+        int[] nmbrs = new int[5];
+        for (int i = 0; i < nmbrs.length; i++) {
+            nmbrs[i] = rand.nextInt(1,10);
         }
     }
 }
