f4e4d461f0c98e816c167d4048ed0f49decb579b
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-12-10 10:59:28 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] toys = {"Bil", "Dinosaurie", "Superman", "Lego", "Nerf"};
+        for (String string : toys) {
+            System.out.println(string);
+        }
+    }
+}
